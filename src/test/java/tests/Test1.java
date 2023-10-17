755263017fc4ac946280cf1d026cee57b275b16a
OmerPc <omer.goldstein.1984@gmail.com>
2023-10-17 16:42:57 +0300

    assertion

--- src/test/java/tests/Test1.java
+++ src/test/java/tests/Test1.java
@@ -1,6 +1,7 @@
 package tests;
 
 import Config.ConfigurationManager;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Test1 {
@@ -8,6 +9,8 @@ public class Test1 {
     @Test
     public void GetEnv()
     {
-        System.out.println(ConfigurationManager.GetInstance("test").GetProperty("url"));
+       String url= ConfigurationManager.GetInstance("test").GetProperty("url");
+        Assert.assertEquals(url,"google.fr","wrong url");
+
     }
 }
