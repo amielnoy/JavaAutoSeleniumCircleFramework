2d58308f51042e1bdaf27b53aa60e3233b9be832
OmerPc <omer.goldstein.1984@gmail.com>
2023-10-17 22:01:44 +0300

    BasePage

---
+++ src/test/java/pages/BasePage.java
@@ -0,0 +1,36 @@
+package pages;
+
+import com.google.common.base.Function;
+import com.google.common.base.Predicate;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class BasePage {
+
+    protected WebDriver driver;
+    protected WebDriverWait wait;
+
+    private BasePage(WebDriver driver)
+    {
+        this.driver=driver;
+        this.wait=new WebDriverWait(driver,30);
+    }
+
+    public void IsElementDisplaaied(By by)
+    {
+        WebElement element=driver.findElement(by);
+        wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    public void IsElementDisplaaiedWithTextIn(By by,String Text)
+    {
+        WebElement element=driver.findElement(by);
+        wait.until(ExpectedConditions.textToBePresentInElement(element,Text));
+    }
+
+
+}
