914857bd3a352236cb547eff0309c14df99dc85d
OmerPc <omer.goldstein.1984@gmail.com>
2023-10-18 12:43:30 +0300

    driver setup

--- src/test/java/Config/BrowserManagment.java
+++ src/test/java/Config/BrowserManagment.java
@@ -2,22 +2,39 @@ package Config;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class BrowserManagment {
-    public static WebDriver SetDrover(){
-        String browser=System.getProperty("browser","ch");
-        ChromeOptions chromeOptions=new ChromeOptions();
-        FirefoxOptions firefoxOptions=new FirefoxOptions();
-        if(browser.equalsIgnoreCase("ch"))
-        {
-            WebDriverManager.chromedriver().setup();
-        }
-        if(browser.equalsIgnoreCase("ff"))
-        {
-            WebDriverManager.chromedriver().setup();
-        }
+    static WebDriver  driver;
 
+    public static WebDriver SetDrover() {
+        String browser = System.getProperty("browser", "ch");
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless");
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.addArguments("--headless");
+
+        if (browser.equalsIgnoreCase("ch")) {
+            WebDriverManager.chromedriver().setup();
+            if(System.getProperty("os.name").toLowerCase().equals("linux")){
+                driver=new ChromeDriver(chromeOptions);
+            }
+            else {
+                driver=new ChromeDriver();
+            }
+        }
+        if (browser.equalsIgnoreCase("ff")) {
+            WebDriverManager.firefoxdriver().setup();
+            if(System.getProperty("os.name").toLowerCase().equals("linux")){
+                driver=new FirefoxDriver(firefoxOptions);
+            }
+            else {
+                driver=new FirefoxDriver();
+            }
+        }
+    return driver;
     }
 }
