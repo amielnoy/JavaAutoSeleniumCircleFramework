a1026d2737fd4d76ac322f07e20356ed6363d606
amiel-melio <amiel@melio.com>
2024-08-28 18:01:55 +0300

    Use local url for debug remove redundant imports

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,21 +1,9 @@
 package tests;
 
 import Config.BrowserManagment;
-import Config.ConfigurationManager;
-import io.qameta.allure.Allure;
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-import org.testng.ITestResult;
 import org.testng.annotations.*;
-import io.github.bonigarcia.wdm.WebDriverManager;
-
-import java.io.ByteArrayInputStream;
-import java.io.File;
 import java.io.IOException;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 
 public class BaseTest {
     protected WebDriver driver;
