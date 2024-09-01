8fc2fd951a72284ae159ad6be5c642660c0f41d4
amiel-melio <amiel@melio.com>
2024-09-01 16:28:11 +0300

    Add data driven test for searching google test results

---
+++ src/test/java/tests/TestCases/GoogleTestDataDrriven.java
@@ -0,0 +1,41 @@
+package tests.TestCases;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import pages.GoogleMainPage;
+import pages.GoogleResultsPage;
+import tests.BaseTest;
+
+import java.util.List;
+
+@Epic("Google regressions")
+@Feature("Search Testing")
+@Listeners(TestListener.class)
+public class GoogleTestDataDrriven extends BaseTest {
+
+    @DataProvider(name = "SearchString")
+    public Object[][] provideGoogleSearchData() {
+        return new Object[][] {
+                {"Selenium"},
+                {"Selenium: Benefits, Uses, Side Effects, Dosage, and More"}
+        };
+    }
+
+    @Test(dataProvider = "SearchString")
+    @Description("Verify search results for different search strings")
+    public void testGoogleSearch(String search) {
+        GoogleMainPage googleMainPage = new GoogleMainPage(getDriver());
+        googleMainPage.enterText(search);
+        GoogleResultsPage googleResultsPage = googleMainPage.SubmitSearch();
+
+        List<String> googleSearchTitelsList=googleResultsPage.getResultTitles();
+        Assert.assertTrue(googleSearchTitelsList.contains(search),
+                "The search value="+search+" is missing from search titles!!");
+    }
+}
+
