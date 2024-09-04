56a5fab1844ec10966e78868576a8d0466d12f1a
amiel-melio <amiel@melio.com>
2024-09-04 11:46:10 +0300

    global constants

---
+++ src/test/java/Config/GlobalConstants.java
@@ -0,0 +1,7 @@
+package Config;
+
+public class GlobalConstants {
+    public static final int MAX_TIMEOUT=30;
+    public static final int MID_TIMEOUT=20;
+    public static final int MIN_TIMEOUT=10;
+}
